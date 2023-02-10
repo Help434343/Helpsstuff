f3728ad2cc23453a027ad9e0cfc6c6aa6e7a2a6c
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-02-09 19:53:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,9 @@
         <title>Help's Stuff</title>
       </Head>
 
-      <main>
+      <body>
         <h1 style = color:white;>"Welcome to Help's Stuff!" <h1/>
         <p style = color:white;>
           No stuff :C
         </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+      </body>
