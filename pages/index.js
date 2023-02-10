8e49a3e5c293f5ae3695b642629c91aeada0ab54
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-02-09 19:52:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
       <Head style = background-color:black;>
         <title>Help's Stuff</title>
       </Head>
