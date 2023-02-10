655ed38673de171ea73a49d7f338d0d811363a41
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-02-09 19:18:47 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to Help's Stuff!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          No stuff :C
         </p>
       </main>
 
