350af16588d6780bc7a4e450418c3d71cdce04cd
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-02-09 19:48:58 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+      <Head style = background-color:black;>
         <title>Help's Stuff</title>
       </Head>
 
       <main>
-        <Header title="Welcome to Help's Stuff!" />
-        <p className="description">
+        <h1 style = color:white;>"Welcome to Help's Stuff!" <h1/>
+        <p style = color:white;>
           No stuff :C
         </p>
       </main>
