bd7eb180ad350d4881ec0e3779b606927a8d0995
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-02-09 19:15:51 -0500

    Changed text to match Help's Stuff

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,11 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Help's Stuff</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Help's Stuff!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
