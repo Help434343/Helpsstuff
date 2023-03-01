e542863324ef1607a197a7bcad17857f53c7ded5
Help434343 <122065536+Help434343@users.noreply.github.com>
2023-03-01 18:20:17 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,5 +8,10 @@
         <p style = color:white;>
           No stuff :C
         </p>
+      <button onclick = stuff()>???<button/>
+<script>
+ Fuction = stuff(){
+            document.write("oops looks like there is stuff")
+<script/>
       </body>
 </html>
